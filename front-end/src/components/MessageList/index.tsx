008891fd7f04d16c0df8752839fb2ed0ef9ee1ea
Douglas Oliveira <douglaspo_97@outlook.com>
2021-10-19 20:58:46 -0300

    create and styling messageList

--- front-end/src/components/MessageList/index.tsx
+++ front-end/src/components/MessageList/index.tsx
@@ -1,3 +1,51 @@
+import logoImg from '../../assets/logo.svg';
+import styles from './styles.module.scss';
+
 export function MessageList() {
-  return <h1>MessageList</h1>;
+  return (
+    <div className={styles.messageListWrapper}>
+      <img src={logoImg} alt="DoWhile 2021" />
+
+      <ul className={styles.messageList}>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos
+            os tempos, vamos pra cima! 🔥🔥
+            <div className={styles.messageUser}>
+              <div className={styles.userImage}>
+                <img src="https://github.com/d0ugui.png" alt="Douglas Oliveira" />
+              </div>
+              <span>Douglas Oliveira</span>
+            </div>
+          </p>
+        </li>
+
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos
+            os tempos, vamos pra cima! 🔥🔥
+            <div className={styles.messageUser}>
+              <div className={styles.userImage}>
+                <img src="https://github.com/d0ugui.png" alt="Douglas Oliveira" />
+              </div>
+              <span>Douglas Oliveira</span>
+            </div>
+          </p>
+        </li>
+
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos
+            os tempos, vamos pra cima! 🔥🔥
+            <div className={styles.messageUser}>
+              <div className={styles.userImage}>
+                <img src="https://github.com/d0ugui.png" alt="Douglas Oliveira" />
+              </div>
+              <span>Douglas Oliveira</span>
+            </div>
+          </p>
+        </li>
+      </ul>
+    </div>
+  );
 }
