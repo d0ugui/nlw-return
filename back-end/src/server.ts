697fa4c6d1f381323ed0676c1b6a501637bec063
Douglas Oliveira <douglaspo_97@outlook.com>
2022-05-05 19:51:51 -0400

    refactor(back-end): nlw-heat to nlw-return

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,4 +1,11 @@
-import { serverHttp } from './app';
+import express from 'express';
 
-//* Definindo porta do servidor
-serverHttp.listen(4000, () => console.log('Server is runnning on port 4000'));
+const app = express();
+
+app.get('/users', (req, res) => {
+  return res.send('aloou')
+})
+
+app.listen(3333, () => {
+  console.log('🚀 Server started at http://localhost:3333')
+})
